5f1f2e3aaa926ba5f8190cb0ec3da78306d62c7a
fsilvestre-2022163 <fsilvestre-2022163@kinal.edu.gt>
2024-10-25 11:03:03 -0600

    proyecto Raylway

---
+++ src/test/java/com/example/Raylway/RaylwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Raylway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaylwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
